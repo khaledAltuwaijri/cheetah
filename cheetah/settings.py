8d54ce8a0af2ed12dad58a115475131aa30a666f
Robert H <robert.t.hwang@gmail.com>
2018-12-02 23:07:44 -0500

    Update local dev db config

--- cheetah/settings.py
+++ cheetah/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'cheetah.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
