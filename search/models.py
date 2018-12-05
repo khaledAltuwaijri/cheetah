d1487e487c33d3bb56de9614580b9865a1568e4c
Robert H <robert.t.hwang@gmail.com>
2018-12-04 22:14:07 -0500

    Add db models prelim

--- search/models.py
+++ search/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+    display_name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    country = models.CharField(max_length=100)
+
+class SearchTrack(models.Model):
+    spotifyID = models.CharField(max_length=200)
+
+class Playlist(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class PlaylistTrack(models.Model):
+    playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    artist = models.CharField(max_length=50)
+    length_ms = models.IntegerField()
