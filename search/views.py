8ba653f3ff1f4095b4f5fe861df81a87ea8d2cf3
Robert H <robert.t.hwang@gmail.com>
2018-12-05 20:44:02 -0500

    Add forms view for search

--- search/views.py
+++ search/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .forms import NameForm
+
 def index(request):
     return HttpResponse("Hello, world. You're at the Search index.")
+
+def getSongRequest(request):
+    if request.method == 'POST':
+        form = NameForm(request.POST)
+    else:
+        form = NameForm()
+
+    return render(request, 'name.html', {'form': form})
