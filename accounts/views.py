63d0e329a99fb82c87f331600b60068572a946e5
Robert H <robert.t.hwang@gmail.com>
2018-12-24 12:28:15 -0500

    Update accounts urls and views, and update the base template to include bootstrap and the navigation bar, and update home:

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,33 @@
-from django.shortcuts import render
 # from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views import generic
+from django.views import View
+from django.contrib import messages
+
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
 
 from users.forms import CustomUserCreationForm
 
-class SignUp(generic.CreateView):
+class SignUp(View):
 
     form_class = CustomUserCreationForm
-    success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            form = CustomUserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                username = form.cleaned_data.get('username')
+                raw_password = form.cleaned_data.get('password1')
+                user = authenticate(username=username, password=raw_password)
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.error(request, "Error")
+        else:
+            form = CustomUserCreationForm()
+        return render(request, 'signup.html', {'form': form})
