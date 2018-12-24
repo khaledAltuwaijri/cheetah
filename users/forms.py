4d5cf4bb2f9b938e1174613e91a5083ab1c18572
Robert H <robert.t.hwang@gmail.com>
2018-12-24 12:26:52 -0500

    Update the login and signup html pages to include bootstrap 4 and update forms and models in users

--- users/forms.py
+++ users/forms.py
@@ -4,9 +4,13 @@ from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
 
+    first_name = forms.CharField(max_length=50, required=False, help_text="Optional")
+    last_name = forms.CharField(max_length=50, required=False, help_text="Optional")
+    email = forms.EmailField(max_length=300, help_text="Email is required")
+
     class Meta(UserCreationForm):
         model = CustomUser
-        fields = ('username', 'email')
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
 
 class CustomUserChangeForm(UserChangeForm):
 
--- users/models.py
+++ users/models.py
@@ -6,5 +6,8 @@ from django.db import models
 
 class CustomUser(AbstractUser):
 
+    location = models.CharField(max_length=30, blank=True)
+    birthday = models.DateField(null=True, blank=True)
+
     def __str__(self):
         return self.email
